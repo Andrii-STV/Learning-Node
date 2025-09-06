79b9bb6d33022aa0495d77ec9738aa9af7112e37
Andrii <stavnichenkoa2000@gmail.com>
2025-09-06 10:43:56 +0300

    also added native fs module to create txt file with the url provided by user

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -12,3 +12,8 @@ var qrSvg = qr.image(siteNameFromUser, {
 
 qrSvg.pipe(fs.createWriteStream('qr-generated.svg'));
 
+
+fs.writeFile('url-provided.txt', siteNameFromUser, 'utf-8', (err) => {
+    if (err) throw err;
+    console.log("QR has been created, URL has been saved in the .txt document");
+});
