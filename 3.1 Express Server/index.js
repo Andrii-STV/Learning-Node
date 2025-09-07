2bcf40164ea5ce0ce08cc72b236693637d29e327
Andrii <stavnichenkoa2000@gmail.com>
2025-09-07 13:53:36 +0300

    Now my app requests url's for pages Home, About, Contact and then sends those files. My challenge is that the app doesn't fetch linked external CSS yet, so I'll learn the way how to do that next thing

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -1,13 +1,27 @@
 import express from "express";
 const app = express();
 const port = 3000;
+const absolutePath = 'D:/Udemy Full-Stack Web Developer course/Learning Node/3.1 Express Server';
 
+app.use(express.static('3.1 Express Server'));
 
 
 app.get('/', (req, res) => {
+    
     //res.send('Well, I guess I will find out how to send page right now');
-    res.sendFile('D:/Udemy Full-Stack Web Developer course/Learning Node/3.1 Express Server/index.html');
+    res.sendFile(absolutePath + '/index.html');
+    // res.send("<h1>Lol it's not even a page, it's only an h1 html tag :)</h1>");
 });
+
+app.get('/about', (req, res) => {
+    res.sendFile(absolutePath + '/about.html');
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(absolutePath + '/contact.html');
+});
+
+
 
 app.listen(port, () => {
     console.log(`App listening port: ${port}`)
