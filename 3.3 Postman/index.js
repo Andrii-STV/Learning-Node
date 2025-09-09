db1a420adf114986a25331ceb350821544fbf0d4
Andrii <stavnichenkoa2000@gmail.com>
2025-09-09 21:25:38 +0300

    Learned 5 main commands: get, post, put. patch and delete. Used Postman to test those commands to see how it sends back statuses

---
+++ 3.3 Postman/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+    res.send('<h1>Hello World</h1>');
+});
+
+app.post('/register', (req, res) =>{
+    res.sendStatus(201);
+});
+
+app.put('/user/andrew', (req, res) =>{
+    res.sendStatus(200);
+});
+
+app.patch('/user/andrew', (req, res) =>{
+    res.sendStatus(200);
+});
+
+app.delete('/user/andrew', (req, res) =>{
+    res.sendStatus(200);
+});
+
+
+app.listen (port, () => {
+    console.log(`Server is running on port ${port}`)
+})
